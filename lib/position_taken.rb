18c724806fe72bdf246bd6e2693f1e2441b25273
Silas Michael Knight <silasfelinus@gmail.com>
2020-10-30 22:09:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  if index == " " or index == ""
-    false
+  if index == " " || index == "" || index == nil
+    return false
   end
 
 end
